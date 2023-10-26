3253c0734387bd8a21826905b103ae9e1173eb26
Abdullah Islam OG <abdullahnj2004@gmail.com>
2023-10-25 20:58:43 -0400

    huihu

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("HI")
+#Abdullah Islam
+
+def encode(password):
+    encoded_num = ''
+    for i in range(8):
+
+        if str(int(password[i])) == '7':
+            encoded_num += '0'
+        elif str(int(password[i])) == '8':
+            encoded_num += '1'
+        elif str(int(password[i])) == '9':
+
+            encoded_num += '2'
+        else:
+            encoded_num += str(int(password[i]) + 3)
+
+    return encoded_num
+
+
+
