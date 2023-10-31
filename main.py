5f519973e48370c6f113b0d56cc9065e74606d22
jchau <jchau509@gmail.com>
2023-10-31 14:46:49 -0400

    yes

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 #Abdullah Islam
 
 def encode(password):
-    encoded_num = ''
+    num = ''
     for i in range(8):
 
         if str(int(password[i])) == '7':
-            encoded_num += '0'
+            num += '0'
         elif str(int(password[i])) == '8':
-            encoded_num += '1'
+            num += '1'
         elif str(int(password[i])) == '9':
 
-            encoded_num += '2'
+            num += '2'
         else:
-            encoded_num += str(int(password[i]) + 3)
+            num += str(int(password[i]) + 3)
 
-    return encoded_num
+    return num
 
 # James Chau
-def decode_rle(rle_data):
-    decode = []
-    for i in range(0, len(rle_data), 2):
-        value = rle_data[i]
-    for t in range(0, value):
-        decode.append(rle_data[i+1])
-    return decode
+def decode(num):
+    for i in num:
+        result = ''
+        for i in num:
+            if i == '2':
+                result += '9'
+            elif i == '1':
+                result += '8'
+            elif i == '0':
+                result += '7'
+            else:
+                result += str(int(i) - 3)
+        return result
